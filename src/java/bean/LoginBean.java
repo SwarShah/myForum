1d61795230b76f6177d673fa0f3382ad603c0b8f
SwarShah <swar12894@gmail.com>
2015-04-17 12:02:30 -0400

    LoginBean with getters and setters

---
+++ src/java/bean/LoginBean.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author c0647456
+ */
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+    private String username = "";
+    private String password = "";
+    private boolean loggedIn = false;
+
+    public LoginBean() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+    
+    
+}
